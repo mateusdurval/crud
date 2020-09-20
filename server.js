85ed86bea0b61d96c897c674480569ee75a58ea8
Mateus Durval <mateus.durvalf@gmail.com>
2020-09-19 21:24:52 -0300

    junção dos crud's da equipe

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ MongoClient.connect(uri, {useUnifiedTopology: true}, (err, client) => {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 
-////////////////////  Table users  //////*/
+//___________________________________  TABLE USERS ________________________________________
 
 //** lists all users in the user table */
 app.get('/', (req, res) => {
@@ -35,7 +35,6 @@ app.get('/users/total', (req, res) => {
         res.status(200).send((result).toString())
     })
 })
-
 //** returns form view for insertion */
 app.get('/users/create', (req, res) => {
     res.render('users/create', {title: 'Cadastrar usuário'})
@@ -50,7 +49,7 @@ app.post('/users/insert', (req, res) =>  {
     })
     console.log(req.body)
 })
-//** returns form view for editing */
+//** return form view for to edit */
 app.route('/users/edit/:id').get((req, res) => {
     var id = req.params.id
     db.collection('users').find({"_id": ObjectId(id)}).toArray((err, result) => {
@@ -111,3 +110,169 @@ app.route('/users/delete/:id').get((req, res) => {
         res.send('success')
     })
 })
+
+//___________________________________ TABLE PRODUCTS ________________________________________
+
+/** lists all users in the user table */
+app.route('/products').get((req, res) => {
+    db.collection('products').find().toArray((err, results) => {
+        if (err) return console.log(err)
+        res.render('products/index.ejs', {data: results, title: 'CRUD - Produtos'})
+    }) 
+})
+//** get total users from db (for ajax request's) */
+app.get('/products/total', (req, res) => {
+    db.collection('products').find().count((err, result) => {
+        res.status(200).send((result).toString())
+    })
+})
+/** return view create */
+app.route('/products/create').get((req, res) => {
+    res.render('products/create.ejs', {title: 'Cadastro de Produtos'})
+})
+/** insert on db */
+app.route('/products/insert').post((req, res) => {
+    db.collection('products').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+
+        console.log ('Inserted in the database!')
+        res.redirect('/products')
+    })
+    console.log(req.body)
+})
+/** retorn view form for to edit */
+app.route('/products/edit/:id').get((req, res) => {
+    var id = req.params.id
+    console.log(id)
+    db.collection('products').find({"_id": ObjectId(id)}).toArray((err, result) => {
+        if (err) return console.log(err) 
+
+        console.log(result)
+
+        res.render('products/edit.ejs', {product: result, title: 'Editar usuário'})
+    })
+})
+/** update user on db */
+app.route('/products/update').post((req, res) => {
+    console.log('ID:', req.body.id)
+
+    var id = req.body.id
+    var name = req.body.name
+    var model = req.body.model
+    var brand = req.body.brand
+    var ean = req.body.ean
+    var made = req.body.made
+    var serial_number = req.body.serial_number
+
+    db.collection('products').updateOne({_id: ObjectId (id)}, {
+        $set: {
+            name: name,
+            model: model,
+            brand: brand,
+            ean: ean,
+            made: made,
+            serial_number: serial_number
+        }
+    }, (err, result) => {
+        if (err) return res.send(err)
+        res.redirect('/products')
+        console.log('Atualizado com sucesso!')
+    })
+})
+/** delete user by ID */
+app.route('/products/destroy/:id').get((req, res) => {
+    console.log('ID:', req.params.id)
+    var id = req.params.id
+
+    db.collection('products').deleteOne({_id: ObjectId(id)}, (err, result) => {
+        if (err) return res.send(500, err)
+
+        console.log('Deleted user!')
+        res.send('success')
+    })
+})
+
+//___________________________________ TABLE COURSE ________________________________________
+
+// return view index and list all users from db
+app.route('/course').get((req, res) => { 
+    db.collection('course').find().toArray((err, results) => {
+        console.log(results)
+        if (err) return console.log(err)
+        res.render('course/index.ejs', {data: results, title: 'CRUD - Aulas'})
+    }) 
+})
+//** get total users from db (for ajax request's) */
+app.get('/course/total', (req, res) => {
+    db.collection('course').find().count((err, result) => {
+        res.status(200).send((result).toString())
+    })
+})
+/** return view form for to create*/
+app.route('/course/create').get((req, res) => {
+    res.render('course/create.ejs', {title: 'Cadastrar Aula'})
+})
+/** insert couse on db */
+app.route('/course/insert').post((req, res) => {
+    db.collection('course').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+
+        console.log ('Inserted in the database!')
+        res.redirect('/course')
+    })
+    console.log(req.body)
+})
+/** return view form for to edit*/
+app.route('/course/edit/:id').get((req, res) => {
+    var id = req.params.id
+    console.log(id)
+    db.collection('course').find({"_id": ObjectId(id)}).toArray((err, result) => {
+        if (err) return console.log(err) 
+
+        console.log(result)
+
+        res.render('course/edit.ejs', {course: result, title: 'Editar usuário'})
+    })
+})
+/** update user on db */
+app.route('/course/update').post((req, res) => {
+    console.log('ID:', req.body.id)
+
+    var id = req.body.id
+    var materia = req.body.materia
+    var meeting = req.body.meeting
+    var time = req.body.time
+    var professor = req.body.professor
+    var group = req.body.group
+    var qtd_students = req.body.qtd_students
+
+    db.collection('course').updateOne({_id: ObjectId (id)}, {
+        $set: {
+            materia: materia,
+            meeting: meeting,
+            time: time,
+            professor: professor,
+            group: group,
+            qtd_students: qtd_students
+        }
+    }, (err, result) => {
+        if (err) return res.send(err)
+        console.log('Atualizado com sucesso!')
+        res.redirect('/course')
+    })
+})
+/** delete user by ID */
+app.route('/course/destroy/:id').get((req, res) => {
+    console.log('ID:', req.params.id)
+    var id = req.params.id
+
+    db.collection('course').deleteOne({_id: ObjectId(id)}, (err, result) => {
+        if (err) return res.send(500, err)
+
+        console.log('Deleted user!')
+        res.send('success')
+    })
+})
+
+
+
